54bb06caec96c770af883f0a97de71be4483a8b4
djkier <djjustinekier@gmail.com>
2026-04-07 20:09:26 +0800

    Add row remove in additional services

--- js/sections/additional-services.js
+++ js/sections/additional-services.js
@@ -1,5 +1,27 @@
 const table = document.querySelector("#additional table");
 const tbody = document.querySelector("#additional tbody");
+const rows = document.querySelectorAll("#additional tbody tr");
 const trashBtns = document.querySelectorAll("#additional .trash-btn");
 const addRowBtn = document.querySelector("add-row-btn");
+
+
+// -----------------------------------------------------
+// DEFAULTS    -----------------------------------------
+// -----------------------------------------------------
+
+trashBtns.forEach(btn => {
+    handleTrashAction(btn)
+});
+
+// -----------------------------------------------------
+// Rows Actions ----------------------------------------
+// -----------------------------------------------------
+
+// const firstInput = parentTr.children[0].querySelector("input");
+function handleTrashAction(btn) {
+    btn.addEventListener("click",  e => {
+        const parentTr = e.target.closest("tr");
+        tbody.removeChild(parentTr);
+    });
+}
 
