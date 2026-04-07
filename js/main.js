3cc64356b3850ef2358ae79d36f50c74c1e61b87
djkier <djjustinekier@gmail.com>
2026-04-07 17:42:55 +0800

    Finalized add row btn and delete row btn

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 import "./sections/patient-details.js ";
 import "./sections/services-packages.js";
+import "./sections/additional-services.js";
 
---
+++ js/sections/additional-services.js
@@ -0,0 +1,5 @@
+const table = document.querySelector("#additional table");
+const tbody = document.querySelector("#additional tbody");
+const trashBtns = document.querySelectorAll("#additional .trash-btn");
+const addRowBtn = document.querySelector("add-row-btn");
+
