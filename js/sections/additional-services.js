cbfa3196dae388293930bf3dd2f73a3df8f3e8f7
djkier <djjustinekier@gmail.com>
2026-04-07 22:21:23 +0800

    Refactor handleNewRow function to make it more readable

--- js/sections/additional-services.js
+++ js/sections/additional-services.js
@@ -1,3 +1,5 @@
+import { nonNegativeInput } from "../util.js";
+
 const table = document.querySelector("#additional table");
 const tbody = document.querySelector("#additional tbody");
 const rows = document.querySelectorAll("#additional tbody tr");
@@ -45,47 +47,60 @@ function handleNewRow() {
     const newTr = document.createElement("tr");
 
     for (let i=0; i < 5; i++) {
+        
+        let tdChild;
+        switch (i) {
+            case 0:
+                tdChild = createInput("name-additional", "text");
+                break;
+            case 1:
+                tdChild = createInput("quantity", "number");
+                break;
+            case 2:
+                tdChild = createInput("unit-price", "number");
+                break;
+            case 3: 
+                tdChild = createInput("amount", "number", true);
+                break;
+            case 4:
+                tdChild = createTrashBtn();
+                break;
+            default:
+                tdChild = document.createElement("div");
+
+        }
+
         const newTd = document.createElement("td");
-        let tdChild = document.createElement("input");
-        tdChild.type = "number"
-        
-        if (i === 0) {
-            tdChild.name = "name-additional";
-            tdChild.type = "text";
-        }
-
-        if (i === 1) {
-            tdChild.name = "quantity";
-        }
-
-        if (i === 2) {
-            tdChild.name = "unit-price"
-        }
-        
-        if (i === 3) {
-            tdChild.name = "amount"
-            tdChild.disabled = true;
-        }
-
-        if (i === 4) {
-            const newImg = document.createElement("img");
-            newImg.src = "./assets/trash.svg";
-            
-            const newDiv = document.createElement("div");
-            newDiv.classList.add("trash-btn");
-            
-            newDiv.appendChild(newImg);
-            handleTrashAction(newDiv);
-
-            tdChild = newDiv;
-        }
-
         newTd.appendChild(tdChild);
-
         newTr.appendChild(newTd);
     }
 
     tbody.appendChild(newTr);
+}
+
+function createInput(name, type, disable = false) {
+    const newInput = document.createElement("input");
+    newInput.name = name;
+    newInput.type = type;
+    if (type == "number") nonNegativeInput(newInput);
+    if (disable) newInput.disabled = disable;
+    
+
+    return newInput;
+}
+
+function createTrashBtn() {
+    const newImg = document.createElement("img");
+    newImg.src = "./assets/trash.svg";
+    
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("trash-btn");
+    
+    newDiv.appendChild(newImg);
+    
+    handleTrashAction(newDiv);
+
+    return newDiv;
 }
 
 
